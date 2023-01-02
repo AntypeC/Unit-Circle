0cae7552cb263365475a52aa0f1dd3d7f32d374a
AntypeC <3c7975@gmail.com>
2023-01-03 03:29:22 +0800

    circle sector not displaying properly

--- index.js
+++ index.js
@@ -19,6 +19,17 @@ document.getElementById('viewport').addEventListener('mousemove', (event) => {
     }
     radian = Math.PI*(degree/180)
     document.getElementsByClassName('pointer')[0].style.transform = 'rotate('+(-degree-90)+'deg)';
+    var sector = document.getElementsByClassName('sector')
+    if (degree <= 180) {
+        sector[0].style.display = 'block';
+        sector[0].style.backgroundImage = "linear-gradient(-"+degree+"deg, transparent 50%, white 50%),linear-gradient("+0+"deg, white 50%, transparent 50%)";
+        sector[1].style.display = 'none';
+    } else {
+        // sector[0].style.display = 'none';
+        sector[0].style.backgroundImage = "linear-gradient(-"+180+"deg, transparent 50%, white 50%),linear-gradient("+0+"deg, white 50%, transparent 50%)";
+        sector[1].style.display = 'block';
+        sector[1].style.backgroundImage = "linear-gradient(-"+degree+"deg, transparent 50%, white 50%),linear-gradient("+180+"deg, white 50%, transparent 50%)";
+    }
     document.getElementById('coords').innerHTML = "P(" + x + ", " + y + ")";
     document.getElementById('angle').innerHTML = 'θ = '+ radian+' rad = '+degree+'°';
     document.getElementById('sin').innerHTML = 'sin(θ) = '+Math.sin(radian);

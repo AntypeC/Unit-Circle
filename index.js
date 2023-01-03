58de728cdc69a6cd4283b8bcb0d0a3a2dfa4a9c5
AntypeC <3c7975@gmail.com>
2023-01-03 15:31:29 +0800

    final commit

--- index.js
+++ index.js
@@ -22,13 +22,13 @@ document.getElementById('viewport').addEventListener('mousemove', (event) => {
     var sector = document.getElementsByClassName('sector')
     if (degree <= 180) {
         sector[0].style.display = 'block';
-        sector[0].style.backgroundImage = "linear-gradient(-"+degree+"deg, transparent 50%, white 50%),linear-gradient("+0+"deg, white 50%, transparent 50%)";
-        sector[1].style.display = 'none';
-    } else {
-        // sector[0].style.display = 'none';
-        sector[0].style.backgroundImage = "linear-gradient(-"+180+"deg, transparent 50%, white 50%),linear-gradient("+0+"deg, white 50%, transparent 50%)";
+        sector[0].style.backgroundImage = "linear-gradient(-"+0+"deg, #FFCCCB 50%, transparent 50%),linear-gradient("+(180-degree)+"deg, transparent 50%, #FFCCCB 50%)";
         sector[1].style.display = 'block';
-        sector[1].style.backgroundImage = "linear-gradient(-"+degree+"deg, transparent 50%, white 50%),linear-gradient("+180+"deg, white 50%, transparent 50%)";
+        sector[1].style.backgroundImage = "linear-gradient(-"+0+"deg, white 50%, transparent 50%),linear-gradient("+180+"deg, transparent 50%, white 50%)";
+        sector[2].style.display = 'none';
+    } else {
+        sector[2].style.display = 'block';
+        sector[2].style.backgroundImage = "linear-gradient("+0+"deg, transparent 50%, #FFCCCB 50%),linear-gradient(-"+degree+"deg, #FFCCCB 50%, transparent 50%)";
     }
     document.getElementById('coords').innerHTML = "P(" + x + ", " + y + ")";
     document.getElementById('angle').innerHTML = 'θ = '+ radian+' rad = '+degree+'°';
